af91d69e84cbe75333dd67b8c1f9f042ae26f122
Rahma Marie Hassan Genady <rahmagenady28@gmail.com>
2026-08-09 03:25:57 +0300

    Create birthday website

---
+++ script.js
@@ -0,0 +1,84 @@
+const surpriseBtn = document.getElementById("surpriseBtn");
+const messageSection = document.getElementById("message");
+
+surpriseBtn.addEventListener("click", () => {
+    messageSection.scrollIntoView({
+        behavior: "smooth"
+    });
+
+    createHearts();
+});
+
+function createHearts() {
+    for (let i = 0; i < 15; i++) {
+        const heart = document.createElement("span");
+
+        heart.innerHTML = "♡";
+        heart.classList.add("floating-heart");
+
+        heart.style.left = Math.random() * 100 + "vw";
+        heart.style.animationDuration =
+            2 + Math.random() * 3 + "s";
+
+        document.body.appendChild(heart);
+
+        setTimeout(() => {
+            heart.remove();
+        }, 5000);
+    }
+}
+const revealElements = document.querySelectorAll(
+    ".photo-card, .message-card, .memories h2, .final"
+);
+
+const observer = new IntersectionObserver(
+    (entries) => {
+        entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add("show");
+            }
+        });
+    },
+    {
+        threshold: 0.15
+    }
+);
+
+revealElements.forEach((element) => {
+    observer.observe(element);
+});
+const giftBtn = document.getElementById("giftBtn");
+const giftMessage = document.getElementById("giftMessage");
+
+giftBtn.addEventListener("click", () => {
+    giftMessage.classList.toggle("show");
+
+    if (giftMessage.classList.contains("show")) {
+        giftBtn.innerHTML = "💝";
+    } else {
+        giftBtn.innerHTML = "🎁";
+    }
+});
+const musicBtn = document.getElementById("musicBtn");
+const musicText = document.getElementById("musicText");
+const birthdayMusic = document.getElementById("birthdayMusic");
+
+musicBtn.addEventListener("click", () => {
+
+    if (birthdayMusic.paused) {
+
+        birthdayMusic.play();
+
+        musicBtn.innerHTML = "⏸️";
+        musicText.textContent = "Music is playing";
+
+    } else {
+
+        birthdayMusic.pause();
+
+        musicBtn.innerHTML = "🎵";
+        musicText.textContent = "Play our song";
+
+    }
+
+});
